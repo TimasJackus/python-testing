e95d6b603a2df67cfc12e9d8d8e7802f244ad9aa
Timas Jackus <tjackus@airblackbox.com>
2020-05-14 15:23:36 +0300

    firefox

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 try:
-    browser = webdriver.Chrome()
+    browser = webdriver.Firefox()
     browser.get(link)
     input1 = browser.find_element_by_name("first_name")
     input1.send_keys("Timas")
