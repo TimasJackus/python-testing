d2c3a8c80566a971f398f2c4471fa676e9b7da2f
Timas Jackus <tjackus@airblackbox.com>
2020-05-16 16:50:43 +0300

    update

--- 1.py
+++ 1.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
 import time
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+link = "https://cntr.click/0zxW7QR"
 try:
     print("STARTING CHROME")
     browser = webdriver.Chrome()
     print("CHROME STARTED...")
     browser.get(link)
-    input1 = browser.find_element_by_name("first_name")
-    input1.send_keys("Timas")
-    input2 = browser.find_element_by_name("last_name")
-    input2.send_keys("Jackus")
-    input3 = browser.find_element_by_class_name("city")
-    input3.send_keys("Vilnius")
-    input4 = browser.find_element_by_id("country")
-    input4.send_keys("Lithuania")
-    button = browser.find_element_by_id("submit_button")
-    button.click()
+    # input1 = browser.find_element_by_name("first_name")
+    # input1.send_keys("Timas")
+    # input2 = browser.find_element_by_name("last_name")
+    # input2.send_keys("Jackus")
+    # input3 = browser.find_element_by_class_name("city")
+    # input3.send_keys("Vilnius")
+    # input4 = browser.find_element_by_id("country")
+    # input4.send_keys("Lithuania")
+    # button = browser.find_element_by_id("submit_button")
+    # button.click()
     h1 = browser.find_element_by_css_selector("h1").text
     print(h1)
 
